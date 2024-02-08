4613bd028411912fcd4d7aabe60e19c6faad1ca7
JerrySio <159001966+JerrySio@users.noreply.github.com>
2024-02-08 14:51:30 +0100

    first commit

---
+++ controllers/controller.php
@@ -0,0 +1,11 @@
+<?php
+switch($action)
+{
+    case 'repose':
+    {
+        echo 'Partie a coder'
+        break;
+    }
+    default:
+    {include 'views/choix.php';}
+}
---
+++ models/model.php
@@ -0,0 +1,23 @@
+
+
+
+
+<?php
+/*exercice 1*/
+$signe = array(
+
+    'Belier' => 'vous devrez faire les efforts les plus sérieux pour acquérir un bon niveau.',
+    'Taureau' => 'Une de vos plus grandes qualités est la capacité de saisir les théories.',
+    'Gémeaux' => 'De temps a autre, admettez que vous n etes pas le seul a raisonner avec logique.',  
+    'Cancer' => 'La rapidité de votre intelligence risque de vous entrainer a négliger certains détails.',
+    'Lion' => 'Un effort vers la patience et la persévérance sera pour vous grandement payant.',
+    'Vierge' => 'Faites l effort de vous concentrer.',
+    'Balance' => 'Vous avez une forte tendance a choisir les solutions astucieuses.',
+    'Scorpion' => 'Renoncez a imposer vos propres données et appliquez les méthodes apprises.',
+    'Sagittaire' => 'Pardonnez avec aisance et sans ambiguité, mais n acceptez aucun extrème.',
+    'Capricorne' => 'Une certaine facilité ne doit pas vous détourner de l objectif.',
+    'Verseau' => 'Ce travail vous demandera beaucoup d ingéniosité.',
+    'Poissons' => 'Votre esprit vif et limpide facilite la résolution des problèmes.',
+
+
+);
